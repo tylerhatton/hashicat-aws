79eaccb93054c14dea511583a02707e0053d8f5c
Tyler Hatton <tyler.hatton@gmail.com>
2022-01-21 18:45:24 +0000

    update bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket_prefix = var.prefix
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-th-123452"
   acl    = "private"
 
   versioning = {
